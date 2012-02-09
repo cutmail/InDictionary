dc19b3b3362f1565d14bfed6c0a1b7683a6155bc
cutmail <sunday.is.over@gmail.com>
2012-02-09 14:38:56 +0900

    Initial Commit

---
+++ InDictionary/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  InDictionary
+//
+//  Created by 荒井 達哉 on 12/02/09.
+//  Copyright (c) 2012年 genesix, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ InDictionary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InDictionary
+//
+//  Created by 荒井 達哉 on 12/02/09.
+//  Copyright (c) 2012年 genesix, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
